88b755e0d8756f18fd900f3f128826175451c0a5
luckyquarter <danielzielinski.nu@gmail.com>
2023-10-02 01:07:39 +0500

    feat: - sets up db for a less strict microservices for a single db for all services
    - adds table upon initialization

--- common/db/session.py
+++ common/db/session.py
@@ -1,8 +1,18 @@
-
 from common.config import Config
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from common.db.base import Base
 
-DATABASE_URL =  Config.DATABASE_URL
+DATABASE_URL = Config.DATABASE_URL
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(engine)
+
+
+# Dependency
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
