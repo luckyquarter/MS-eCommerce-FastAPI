f32e15dc1cb7ed5232bc3ec794b66818856bd299
luckyquarter <danielzielinski.nu@gmail.com>
2023-10-04 15:45:24 +0500

    refactor: formalize fields to create products

--- common/db/models/products.py
+++ common/db/models/products.py
@@ -1,20 +1,33 @@
 # common_db/models/products.py
-from sqlalchemy import Column, Integer, String, Float, ForeignKey,DateTime
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from common.db.base import Base
+from sqlalchemy import Enum
+import datetime
+from common.Enums.product_enums import ProductStatus
+from common.Enums.product_enums import ProductCategory
 
 
 class Product(Base):
     __tablename__ = "products"
 
     id = Column(Integer, primary_key=True, index=True)
-    # category_id = Column(Integer, ForeignKey("categories.category_id"))
+    category_name = Column(Enum(ProductCategory), ForeignKey("categories.name"))
     name = Column(String(60), index=True)
     description = Column(String(255))
     price = Column(Float)
     image = Column(String(255))
-
-    # category = relationship("Category", back_populates="products")
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
+    status = Column(
+        Enum(
+            ProductStatus,
+            name="product_status",
+            create_type=True,
+        ),
+        default=ProductStatus.ACTIVE.value,
+    )
+    category = relationship("Category", back_populates="products")
 
     # @classmethod
     # def get_by_id(cls, product_id: int):
