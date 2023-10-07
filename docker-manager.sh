6b93575b454b11ee8047ad40b466f930863e8f6c
luckyquarter <danielzielinski.nu@gmail.com>
2023-10-07 22:21:22 +0500

    refactor: docker start up

--- docker-manager.sh
+++ docker-manager.sh
@@ -3,11 +3,13 @@
 # Define the paths to the Docker Compose files
 DB_COMPOSE_FILE="docker-compose-db.yml"
 APP_COMPOSE_FILE="docker-compose-product-service.yml"
+SALES_COMPOSE_FILE="docker-compose-sales-service.yml"
 
 # Function to stop all containers
 stop_containers() {
   docker-compose -f "$APP_COMPOSE_FILE" down
   docker-compose -f "$DB_COMPOSE_FILE" down
+  docker-compose -f "$SALES_COMPOSE_FILE" down
 }
 
 # Function to start all containers with environment variables
@@ -21,9 +23,14 @@ start_containers() {
     export $(cat .env.app | xargs)
   fi
 
+  if [ -f .env.sales ]; then
+    export $(cat .env.sales | xargs)
+  fi
+
   # Start the containers
   docker-compose -f "$DB_COMPOSE_FILE" up -d 
   docker-compose -f "$APP_COMPOSE_FILE" up -d 
+  docker-compose -f "$SALES_COMPOSE_FILE" up -d 
 }
 
 # Main script logic
